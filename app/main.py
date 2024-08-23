ddc8d5a2b50f469a283be7e59e4a754d24b3e11c
Nick <tinman346@gmail.com>
2024-08-23 10:19:13 -0400

    added defeault root

--- app/main.py
+++ app/main.py
@@ -24,3 +24,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
